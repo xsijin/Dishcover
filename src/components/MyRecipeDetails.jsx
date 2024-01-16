9c719b49573c0e5e746c678b3508cc51faf2a371
Haozhi <zhi_0415@hotmail.com>
2024-01-16 14:46:29 +0800

    1. Added some basic css. 2. Added picture url field.

--- src/components/MyRecipeDetails.jsx
+++ src/components/MyRecipeDetails.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
+
 function MyRecipeDetails() {
     const params = useParams();
     const [recipeDetails, setRecipeDetails] = useState(null);
@@ -39,13 +40,15 @@ function MyRecipeDetails() {
     return (
         <div>
             {recipeDetails && (
-                <div>
-                    <h1>{recipeDetails.title}</h1>
-                    <p>Tags: {recipeDetails.tags.join(', ')}</p>
-                    <p>Ingredients: {recipeDetails.ingredients.join(', ')}</p>
-                    <p>Prep Time: {recipeDetails.preptime}</p>
-                    <p>Instructions: {recipeDetails.instructions}</p>
-                    <button onClick={handleEditButtonClick}>Edit Recipe</button>
+                <div className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <h1 className="text-gray-300 font-bold text-xl mb-2">{recipeDetails.title}</h1>
+                    <p className="mb-2 font-bold text-gray-300">Author: </p><span className="text-gray-300">{ }</span>
+                    <p className="font-bold text-gray-300">Tags: </p><span className="text-gray-300">{recipeDetails.tags.join(', ')}</span>
+                    <p className="font-bold text-gray-300">Ingredients: </p><span className="text-gray-300">{recipeDetails.ingredients.join(', ')}</span>
+                    <p className="mb-2 font-bold text-gray-300">Prep Time: </p><span className="text-gray-300">{recipeDetails.preptime}</span>
+                    <p className="mb-2 font-bold text-gray-300">Instructions: </p><span className="text-gray-300">{recipeDetails.instructions}</span>
+                    <p className="mb-2 font-bold text-gray-300">Picture URL: </p><span className="text-gray-300">{recipeDetails.picture_url}</span>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleEditButtonClick}>Edit Recipe</button>
                 </div>
             )}
         </div>
