f1732de0fda14221e464bc4d440f7dcedf769912
sijin <sijincag@gmail.com>
2024-01-19 16:39:32 +0800

    Add routing from recipe to review

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import BreadCrumb from './components/BreadCrumb/BreadCrumb';
-import UserProfile from './components/UserProfile/UserProfile';
-import ReviewPage from './components/Reviews/ReviewPage';
-import MyRecipes from './components/MyRecipes';
-import MyRecipeDetails from './components/MyRecipeDetails';
-import MyRecipeEditing from './components/MyRecipeEditing';
-import PublicLandingPage from './components/PublicLandingPage';
-import PublicRecipeDetails from './components/PublicRecipeDetails';
+import BreadCrumb from "./components/BreadCrumb/BreadCrumb";
+import UserProfile from "./components/UserProfile/UserProfile";
+import ReviewPage from "./components/Reviews/ReviewPage";
+import MyRecipes from "./components/MyRecipes";
+import MyRecipeDetails from "./components/MyRecipeDetails";
+import MyRecipeEditing from "./components/MyRecipeEditing";
+import PublicLandingPage from "./components/PublicLandingPage";
+import PublicRecipeDetails from "./components/PublicRecipeDetails";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -23,17 +23,20 @@ function App() {
       </nav>
       <main>
         <Routes>
+          <Route path="/" element={<PublicLandingPage />} />
           <Route path="/users/show" element={<UserProfile />} />
-          <Route path="/ReviewPage" element={<ReviewPage />} />
+          <Route path="/reviews/:recipeId" element={<ReviewPage />} />
           <Route path="/MyRecipes" element={<MyRecipes />} />
           <Route path="/MyRecipeDetails/:id" element={<MyRecipeDetails />} />
           <Route path="/MyRecipeEditing/:id" element={<MyRecipeEditing />} />
-          <Route path="/" element={<PublicLandingPage />} />
-          <Route path="/PublicRecipeDetails/:id" element={<PublicRecipeDetails />} />
+          <Route
+            path="/PublicRecipeDetails/:id"
+            element={<PublicRecipeDetails />}
+          />
         </Routes>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/BreadCrumb/BreadCrumb.jsx
+++ src/components/BreadCrumb/BreadCrumb.jsx
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 export default function BreadCrumb() {
   return (
     <nav>
+      <Link to="/">Home</Link>
+      &nbsp; | &nbsp;
       <Link to="/users/show">User Profile</Link>
       &nbsp; | &nbsp;
-      <Link to="/ReviewPage">ReviewPage</Link>
-      &nbsp; | &nbsp;
       <Link to="/MyRecipes">My Recipes</Link>
-      &nbsp; | &nbsp;
-      <Link to="/">Home</Link>
+
     </nav>
   );
 }
