968c28512428aa78f852de320046c6f19c51daf3
sijin <sijincag@gmail.com>
2024-01-22 21:38:15 +0800

    Experimental - make navbar look nicer

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,23 +13,53 @@ export default function NavBar() {
     <div className="navbar stayfixed bg-base-100">
       <div className="flex-1">
         {/* Back button */}
-        <button className="btn btn-ghost text-xl" onClick={handleGoBack}>
+        <button
+          className="btn btn-ghost text-xl btn-circle"
+          onClick={handleGoBack}
+        >
           &lt;
         </button>
+
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to="/">Homepage</Link>
+            </li>
+            <li>
+              <Link to="/MyRecipes">Make a Dishcovery</Link>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+          </ul>
+        </div>
+
         {/* Home button */}
         <Link to="/" className="btn btn-ghost text-xl">
           Dishcover
         </Link>
       </div>
 
-      <div className="flex-1 flex justify-center">
-        {/*Add new recipe button*/}
-        <Link to="/MyRecipes" className="btn btn-ghost text-xl">
-          + Add a Dishcovery
-        </Link>
-      </div>
-
-      <div className="flex-none gap-2">
+      <div className="flex-1 gap-2">
         <div className="form-control">
           <div className="dropdown dropdown-bottom dropdown-end">
             <div
